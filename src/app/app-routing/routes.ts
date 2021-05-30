b9de74fe2e0498fbec4c69f6d82499cfb4f9b5b7
Jitu-Niagra <zolasmandela4@gmail.com>
2021-05-30 18:24:22 +0300

    Single Page Application Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from "@angular/router";
+import { AboutComponent } from "../about/about.component";
+import { FooterComponent } from "../footer/footer.component";
 import { HomeComponent } from "../home/home.component";
 import { MenuComponent } from "../menu/menu.component";
 import { ContactComponent } from './../contact/contact.component';
+import { DishdetailComponent } from './../dishdetail/dishdetail.component';
 
 export const routes:Routes=[
     {path:'home',component:HomeComponent},
     {path:'menu',component:MenuComponent},
     {path:'',redirectTo: '/home',pathMatch:'full'},
-    {path:'contactus',component:ContactComponent}
+    {path:'contactus',component:ContactComponent},
+    {path:'about',component:AboutComponent},
+    {path:'dishdetail/:id',component:DishdetailComponent },
+    {
+        path:'footer',
+        component:FooterComponent
+    }
 
 ]
